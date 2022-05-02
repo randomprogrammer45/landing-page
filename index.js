c72306eb2be2fcc01531ba4833e52f4685494941
randomprogrammer45 <95759928+randomprogrammer45@users.noreply.github.com>
2022-05-02 20:01:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let body = document.querySelector("body")
+document.getElementById("theLast").onclick = () => {
+window.alert("thanks, you saved our company!")
+}
+document.getElementById("firstButton").onclick = () => {
+alert("thanks for signing up")
+}
